485b421f68a59f5d30aad561016cccc714915760
[Cloud3] 이찬형 <lchyeong@gmail.com>
2024-06-05 20:29:12 +0900

    refactor: router 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 
-import Cart from './pages/cart/Cart';
-import Category from './pages/manager/category/Category';
 import Footer from './components/Footer';
 import Header from './components/header/Header';
-import Join from './pages/join/Join';
-import Login from './pages/login/Login';
-import Main from './pages/main/Main';
-import MyPage from './pages/myPage/MyPage';
-import Order from './pages/order/Order';
-import Product from './pages/manager/product/Product';
-import ProductDetail from './pages/productDetail/ProductDetail';
-import ProductList from './pages/productList/ProductList';
+import Router from './Router';
 
 function App() {
   const theme = createTheme({
@@ -37,22 +28,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Header />
-
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/order" element={<Order />} />
-            <Route
-              path="/product/list/:category1/:category2?"
-              element={<ProductList />}
-            />
-            <Route path="/product/detail" element={<ProductDetail />} />
-            <Route path="/product/management" element={<Product />} />
-            <Route path="/category/management" element={<Category />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/myPage" element={<MyPage />} />
-          </Routes>
+          <Router />
           <Footer />
         </BrowserRouter>
       </ThemeProvider>
---
+++ src/Router.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Cart from './pages/cart/Cart';
+import Category from './pages/manager/category/Category';
+import Join from './pages/join/Join';
+import Login from './pages/login/Login';
+import Main from './pages/main/Main';
+import MyPage from './pages/myPage/MyPage';
+import Order from './pages/order/Order';
+import Product from './pages/manager/product/Product';
+import ProductDetail from './pages/productDetail/ProductDetail';
+import ProductList from './pages/productList/ProductList';
+
+const Router = () => {
+    return (<Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/order" element={<Order />} />
+        <Route
+          path="/product/list/:category1/:category2?"
+          element={<ProductList />}
+        />
+        <Route path="/product/detail" element={<ProductDetail />} />
+        <Route path="/product/management" element={<Product />} />
+        <Route path="/category/management" element={<Category />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/myPage" element={<MyPage />} />
+    </Routes>)
+}
+export default Router
