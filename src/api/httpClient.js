082912d893c24f7f55870f728a69882dc0b69d14
[Cloud3] 오재영 <wodud.oh@gmail.com>
2024-06-11 23:48:02 +0900

    fix: jwt 토큰 만료시 재발급 기능 수정

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { refreshAccessToken } from './authApi';
+import useUserStore from '../store/useUserStore';
 
 export const httpClient = axios.create({
   // eslint-disable-next-line
@@ -37,14 +38,22 @@ httpClient.interceptors.response.use(
     if (status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const newAccessToken = await refreshAccessToken();
+        const setUser = useUserStore((state) => state.setUser);
+        const response = await refreshAccessToken();
+        const { newAccessToken, name, email, role } = response.data;
+        console.log('response.data:' + response.data);
+        // Access Token을 로컬 스토리지에 저장
         localStorage.setItem('accessToken', newAccessToken);
+        // Zustand 상태 업데이트
+        setUser({ name, email, role });
         originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
         return httpClient(originalRequest);
       } catch (refreshError) {
         console.error('Token refresh failed:', refreshError);
         // 로그아웃 처리 등 추가적인 처리가 필요할 수 있음
         return Promise.reject(refreshError);
+      } finally {
+        navigator('/');
       }
     }
     return Promise.reject(error);
