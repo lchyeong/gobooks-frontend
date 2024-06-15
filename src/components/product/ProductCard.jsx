39a8cdb5ba80cde47c5c47f0a127f951da139482
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-06-15 20:06:47 +0900

    Feat: product list -> product detail 링크 연결

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -6,11 +6,12 @@ import {
   Skeleton,
   Typography,
 } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom'; // Link 컴포넌트 import
 
 import noImage from '../../pages/productList/images/noimage.jpg'; // 기본 이미지 추가
 
-function ProductCard({ product }) {
+function ProductCard({product}) {
   const [isLoading, setIsLoading] = useState(true);
   const [imageUrl, setImageUrl] = useState(noImage); // 기본 이미지로 초기화
 
@@ -30,56 +31,59 @@ function ProductCard({ product }) {
   }, [product.id, product.imageUrl]);
 
   const formattedPrice =
-    product.fixedPrice !== undefined
-      ? product.fixedPrice.toLocaleString()
-      : 'N/A';
+      product.fixedPrice !== undefined
+          ? product.fixedPrice.toLocaleString()
+          : 'N/A';
   return (
-      <Card
-          sx={{
-            borderRadius: '8px',
-            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-            transition: 'box-shadow 0.3s ease',
-            '&:hover': {
-              boxShadow: '0px 0px 8px #ABA5F3',
-            },
-            height: '100%'
-          }}
-      >
-        <div className="tw-relative tw-pb-[142%]">
-        {' '}
-        {/* 책 표지 비율 유지 */}
-        {isLoading ? (
-          <Skeleton variant="rectangular" width="100%" height="100%" />
-        ) : (
-          <CardMedia
-            component="img"
+      <Link to={`/product/detail/${product.id}`}>
+        <Card
             sx={{
-              height: 250,
-              objectFit: 'cover',
+              borderRadius: '8px',
+              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+              transition: 'box-shadow 0.3s ease',
+              '&:hover': {
+                boxShadow: '0px 0px 8px #ABA5F3',
+              },
+              height: '100%'
             }}
-            className="tw-absolute tw-top-0 tw-left-0 tw-object-cover tw-h-full tw-w-full"
-            image={imageUrl}
-            alt={product.title || 'Product'}
-          />
-        )}
-      </div>
-      <CardContent className="tw-p-4">
-        <Typography
-          variant="h6"
-          component="div"
-          className="tw-mb-2 tw-line-clamp-2"
         >
-          {product.title || 'Unnamed Product'}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" className="tw-mb-2">
-          {product.author || 'Unknown Author'} /{' '}
-          {product.publisher || 'Unknown Publisher'}
-        </Typography>
-        <Typography variant="h6" component="div" className="tw-font-bold">
-          {formattedPrice}원
-        </Typography>
-      </CardContent>
-    </Card>
+          <div className="tw-relative tw-pb-[142%]">
+            {' '}
+            {/* 책 표지 비율 유지 */}
+            {isLoading ? (
+                <Skeleton variant="rectangular" width="100%" height="100%"/>
+            ) : (
+                <CardMedia
+                    component="img"
+                    sx={{
+                      height: 250,
+                      objectFit: 'cover',
+                    }}
+                    className="tw-absolute tw-top-0 tw-left-0 tw-object-cover tw-h-full tw-w-full"
+                    image={imageUrl}
+                    alt={product.title || 'Product'}
+                />
+            )}
+          </div>
+          <CardContent className="tw-p-4">
+            <Typography
+                variant="h6"
+                component="div"
+                className="tw-mb-2 tw-line-clamp-2"
+            >
+              {product.title || 'Unnamed Product'}
+            </Typography>
+            <Typography variant="body2" color="textSecondary"
+                        className="tw-mb-2">
+              {product.author || 'Unknown Author'} /{' '}
+              {product.publisher || 'Unknown Publisher'}
+            </Typography>
+            <Typography variant="h6" component="div" className="tw-font-bold">
+              {formattedPrice}원
+            </Typography>
+          </CardContent>
+        </Card>
+      </Link>
   );
 }
 
